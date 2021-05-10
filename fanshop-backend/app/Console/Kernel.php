c8a5c1c4ee0c9a17f96d5ab05050c38d656bda6c
willian95 <rodriguezwillian95@gmail.com>
2021-05-10 04:58:32 -0400

    cambios

--- fanshop-backend/app/Console/Kernel.php
+++ fanshop-backend/app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('order:retrieve')->everyMinute();
+        $schedule->command('order:retrieve')->everyThirtyMinutes();
     }
 
     /**
